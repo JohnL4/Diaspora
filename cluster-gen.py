13fb5c5edaa814bb3a722f972d3b3e29938bfa56
John Lusk <john-github@how-hard-can-it-be.com>
2013-03-28 01:55:07 -0400

    Make legend optional with --legend command-line argument. Added slipstream guarantee, which required generating systems before printing them out (previously, "generate" and "print" were happening in the same loop).

--- cluster-gen.py
+++ cluster-gen.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+                                        # -*- fill-column: 120 -*-
 
 # Generates a graph of systems for a Diaspora RPG cluster.
 #
@@ -8,6 +9,7 @@
 #     cluster-gen.py A B C D E F > our-cluster.dot
 #     neato -Tpdf -oour-cluster.pdf our-cluster.dot
 
+import argparse
 import random
 import sys
 
@@ -22,53 +24,8 @@ def fudgeThrow():
         throw += random.randrange(-1,2)
     return throw
 
-def nodeColor_Subtractive( techThrow, envThrow, resThrow):
-    """Returns the background color a node should have, using a different algorithm than that of nodeColor_Additive."""
-    techColor = {"red":1, "green":1, "blue":0}
-    envColor  = {"red":0, "green":0, "blue":1}
-    resColor  = {"red":1, "green":0, "blue":0}
-
-    techness = (techThrow + 4)/8
-    envness = (envThrow + 4)/8
-    resness = (resThrow + 4)/8
-    # print("\t\tDEBUG: techness = {0}, envness = {1}, resness = {2}".format( techness, envness, resness), file=sys.stderr)
-
-    r = g = b = 1
-
-    i = 0
-    for aspect in [[techness, techColor], [envness, envColor], [resness, resColor]]:
-        r = r - (1 - aspect[0]) * aspect[1]["red"]
-        g = g - (1 - aspect[0]) * aspect[1]["green"]
-        b = b - (1 - aspect[0]) * aspect[1]["blue"]
-        # print("\t\tDEBUG: after aspect {0}, (r, g, b) = ({1}, {2}, {3})".format(i, r, g, b), file=sys.stderr)
-        i = i + 1
-
-    # Fix problem where we might have taken too much off.
-    mr = mg = mb = 0                    # Max. red, green, blue, if a system is T4 E4 R4.
-    for c in [techColor, envColor, resColor]:
-        mr = mr + c["red"]
-        mg = mg + c["green"]
-        mb = mb + c["blue"]
-    m = max( mr, mg, mb)                # m is the most we could have taken from 1.  Need to make this zero.
-
-    r = (r + (m - 1)) / m               # m = 2 ==> [-1,1] --> [0,2] --> [0,1]
-    g = (g + (m - 1)) / m
-    b = (b + (m - 1)) / m
-
-    # print("\tDEBUG: T{0} E{1} R{2} ==> color({3}, {4}, {5})".format( techThrow, envThrow, resThrow, r, g, b),file=sys.stderr)
-
-    # Make hex RGB color
-    base = 127
-    r = int( base + (255 - base) * r)
-    g = int( base + (255 - base) * g)
-    b = int( base + (255 - base) * b)
-
-    retval = "#{0:02x}{1:02x}{2:02x}".format( r, g, b)
-
-    return retval
-
 def nodeColor_Additive(techThrow, envThrow, resThrow):
-    """Returns the background color a node should have."""
+    """Returns the background color a node should have, using a fairly simple additive algorithm."""
 
     techness = (techThrow + 4)/8
     envness = (envThrow + 4)/8
@@ -105,63 +62,111 @@ def nodeColor_Additive(techThrow, envThrow, resThrow):
 
     return retval
 
-def nodeColor_Additive2(techThrow, envThrow, resThrow):
-    """Returns the background color a node should have."""
-    techColor = {"red":1, "green":1, "blue":0}
-    envColor  = {"red":0, "green":0, "blue":1}
-    resColor  = {"red":1, "green":0, "blue":0}
+# -------------------------------------------------  class StarSystem  -------------------------------------------------
 
-    techness = (techThrow + 4)/8
-    envness = (envThrow + 4)/8
-    resness = (resThrow + 4)/8
+class StarSystem:
+    """A star system."""
+
+    def __init__( self, aName):
+        self._name = aName
+        self._techLevel = fudgeThrow()
+        self._envLevel = fudgeThrow()
+        self._resLevel = fudgeThrow()
+        
+    def getName( self):
+        return self._name
     
-    r = g = b = 0
+    def getScore( self):
+        """System's "score": sum of tech, env and res levels."""
+        return self._techLevel + self._envLevel + self._resLevel
 
-    for aspect in [[techness, techColor], [envness, envColor], [resness, resColor]]:
-        r = r + aspect[0] * aspect[1]["red"]
-        g = g + aspect[0] * aspect[1]["green"]
-        b = b + aspect[0] * aspect[1]["blue"]
+    def getTechLevel( self):
+        return self._techLevel
 
-    # Scale back to interval [0,1]
-    m = 1 # max(r,g,b)     # Max. it could possibly be, given the static color setup above.
-    mr = mg = mb = 0                    # Max. red, green, blue, if a system is T4 E4 R4.
-    for c in [techColor, envColor, resColor]:
-        mr = mr + c["red"]
-        mg = mg + c["green"]
-        mb = mb + c["blue"]
-    m = max( mr, mg, mb)                # m is the max. that could have been added.
+    def setTechLevel( self, aTechLevel):
+        self._techLevel = aTechLevel
+        
+    def getEnvLevel( self):
+        return self._envLevel
 
-    r = min( r, 1)
-    g = min( g, 1)
-    b = min( b, 1)
+    def getResLevel( self):
+        return self._resLevel
 
-    # print("\tDEBUG: T{0} E{1} R{2} ==> color({3}, {4}, {5})".format( techThrow, envThrow, resThrow, r, g, b),file=sys.stderr)
+    def toString( self):
+        return "{0}: T{1} E{2} R{3}, score= {4}".format(
+            self._name, self._techLevel, self._envLevel, self._resLevel, self.getScore())
 
-    # Make hex RGB color
-    base = 127 + 32
-    r = int( base + (255 - base) * r)
-    g = int( base + (255 - base) * g)
-    b = int( base + (255 - base) * b)
-
-    retval = "#{0:02x}{1:02x}{2:02x}".format( r, g, b)
-
-    return retval
-
-# ----------------------------------  main  ----------------------------------
+# -------------------------------------------------------  main  -------------------------------------------------------
 
 nodeColor_func = nodeColor_Additive
 random.seed()
 
-systems = sys.argv
-systems.pop(0)                          # zap program name
-n = len(systems)                        # Number of systems
+argParser = argparse.ArgumentParser(description="Diaspora cluster generator")
+argParser.add_argument('--legend', action="store_true", help="Include a legend of colors in the generated graph")
+argParser.add_argument('systemNames', nargs=argparse.REMAINDER, help="Unique first letters of star system names")
 
-connected = list(range(n))              # Whether system i is connected to the
-                                        # cluster yet.
+args = argParser.parse_args()
+
+# systemNames = sys.argv
+# systemNames.pop(0)                          # zap program name
+
+systemNames = args.systemNames
+
+n = len(systemNames)                    # Number of systems
+
+connected = list(range(n))              # Whether system i is connected to the cluster yet.
 
 for i in range(n):
     connected[i] = 0
 
+starSystems = []
+maxTech = -5                            # Something less than -4
+maxScore = -15                          # Something less than 3*(-4)
+minScore = 15                           # Something greater than 3*4
+for i in range(n):
+    starSys = StarSystem( systemNames[i])
+    starSystems.append( starSys)
+    if (maxTech < starSys.getTechLevel()):
+        maxTech = starSys.getTechLevel()
+    s = starSys.getScore()
+    if (minScore > s):
+        minScore = s
+    if (maxScore < s):
+        maxScore = s
+
+print("\tDEBUG: systems before checking for slipstream guarantee:", file=sys.stderr)
+for starSys in starSystems:
+    print( "\tDEBUG: {0}".format( starSys.toString()), file=sys.stderr)
+    
+goodSystems = []
+crappySystems = []
+if (maxTech < 2):
+    # Must fulfill "slipsteam guarantee": at least one system of T2 or better.
+    print('\tDEBUG: **** Max tech ({0}) < 2'.format( maxTech), file=sys.stderr)
+    for starSys in starSystems:
+        s = starSys.getScore()
+        if (s == maxScore):
+            goodSystems.append( starSys)
+        if (s == minScore):
+            crappySystems.append( starSys)
+    print('\tDEBUG: good systems: {0}'.format( ", ".join(map( lambda s : s.getName(), goodSystems))), file=sys.stderr)
+    if (len(goodSystems) == 1):
+        selectedGoodSystem = goodSystems[0]
+    else:
+        selectedGoodSystem = goodSystems[random.randrange(len(goodSystems))]
+    print('\tDEBUG: selected good system: {0}'.format( selectedGoodSystem.getName()), file=sys.stderr)
+    selectedGoodSystem.setTechLevel( 2)
+
+    print('\tDEBUG: crappy systems: {0}'.format( ", ".join(map( lambda s : s.getName(), crappySystems))), file=sys.stderr)
+    if (len(crappySystems) == 1):
+        crappySystems[0].setTechLevel( 2)
+    else:
+        # On the off chance that all the systems have the same score, take the "good" system out of the crappy list.
+        goodSystemName = selectedGoodSystem.getName()
+        crappySystems = list(filter( lambda s : s.getName() != goodSystemName, crappySystems))
+        print('\tDEBUG: crappy systems after filtering out good system (if present): {0}'.format( ", ".join(map( lambda s : s.getName(), crappySystems))), file=sys.stderr)
+        crappySystems[random.randrange(len(crappySystems))].setTechLevel( 2)
+        
 print( '''// Process with GraphViz neato.
 graph {
 graph [start=1
@@ -172,17 +177,27 @@ graph [start=1
 node [shape=circle, style=filled]
 ''')
 
-# Legend
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("All", 4, 4, 4, nodeColor_func(4,4,4)))
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Tech", 4, -4, -4, nodeColor_func(4,-4,-4)))
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Environment", -4, 4, -4, nodeColor_func(-4,4,-4)))
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Resources", -4, -4, 4, nodeColor_func(-4,-4,4)))
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("None", -4, -4, -4, nodeColor_func(-4,-4,-4)))
+if (args.legend):
+    print( '// Legend')
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("All", 4, 4, 4, nodeColor_func(4,4,4)))
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Tech", 4, -4, -4, nodeColor_func(4,-4,-4)))
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Environment", -4, 4, -4, nodeColor_func(-4,4,-4)))
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("Resources", -4, -4, 4, nodeColor_func(-4,-4,4)))
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format("None", -4, -4, -4, nodeColor_func(-4,-4,-4)))
 
-legendNodes = ["All","Tech","Environment","Resources","None"]
-for i in range(len(legendNodes)-1):
-    for j in range(i+1, len(legendNodes)):
-        print( '{0} -- {1}'.format(legendNodes[i], legendNodes[j]))
+    legendNodes = ["All","Tech","Environment","Resources","None"]
+    for i in range(len(legendNodes)-1):
+        for j in range(i+1, len(legendNodes)):
+            print( '{0} -- {1}'.format(legendNodes[i], legendNodes[j]))
+    print( '// Legend ends')
+    print()
+
+for starSys in starSystems:
+    t = starSys.getTechLevel()
+    e = starSys.getEnvLevel()
+    r = starSys.getResLevel()
+    color = nodeColor_func( t, e, r)
+    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format(starSys.getName(), t, e, r, color))
 print()
 
 # Need to roll for every system but the last two.  2nd-to-last is guaranteed
@@ -190,41 +205,25 @@ print()
 
 for i in range(n-2):
     connectedThrow = fudgeThrow()
-    # print("\tDEBUG: {0} ({1}): {2}".format(i, systems[i], connectedThrow), file=sys.stderr)
-    techThrow = fudgeThrow()            # Technology level
-    envThrow = fudgeThrow()             # Environment level
-    resThrow = fudgeThrow()             # Resources level
-    color = nodeColor_func(techThrow, envThrow, resThrow)
-    print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format(systems[i], techThrow, envThrow, resThrow, color))
-    print( "{0} -- {1}".format(systems[i], systems[i+1]))
+    print("\tDEBUG: {0} ({1}): {2}".format(i, starSystems[i].getName(), connectedThrow), file=sys.stderr)
+    print( "{0} -- {1}".format(starSystems[i].getName(), starSystems[i+1].getName()))
     j = i + 2
     if (connectedThrow >= 0):
         while ((j < n) and connected[j]):
             j = j+1
         if (j < n):
-            print("{0} -- {1}".format(systems[i], systems[j]))
+            print("{0} -- {1}".format(starSystems[i].getName(), starSystems[j].getName()))
             connected[j] = 1
     if (connectedThrow > 0):
         j = j+1
         while ((j < n) and connected[j]):
             j = j+1
         if (j < n):
-            print("{0} -- {1}".format(systems[i], systems[j]))
+            print("{0} -- {1}".format(starSystems[i].getName(), starSystems[j].getName()))
             connected[j] = 1
 
-# print("\tDEBUG: {0} ({1}): Last".format(n-2, systems[n-2]), file=sys.stderr)
+print("\tDEBUG: {0} ({1}): Last".format(n-2, starSystems[n-2].getName()), file=sys.stderr)
 
-techThrow = fudgeThrow()                
-envThrow = fudgeThrow()                 
-resThrow = fudgeThrow()                 
-color = nodeColor_func(techThrow, envThrow, resThrow)
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format(systems[n-2], techThrow, envThrow, resThrow, color))
-print("{0} -- {1}".format(systems[n-2], systems[n-1]))
-
-techThrow = fudgeThrow()                
-envThrow = fudgeThrow()                 
-resThrow = fudgeThrow()                 
-color = nodeColor_func(techThrow, envThrow, resThrow)
-print( '{0} [label="\\N\\nT{1} E{2} R{3}", fillcolor="{4}"]'.format(systems[n-1], techThrow, envThrow, resThrow, color))
+print("{0} -- {1}".format(starSystems[n-2].getName(), starSystems[n-1].getName()))
 
 print( "}")                             # graph
