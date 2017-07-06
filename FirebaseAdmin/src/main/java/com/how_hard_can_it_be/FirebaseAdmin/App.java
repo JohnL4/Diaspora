8fecbf39cdb385d59529da48aa350262455b794e
John Lusk <john-github@how-hard-can-it-be.com>
2017-07-06 19:14:48 -0400

    Add hookup to Firebase d/b.

--- FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
+++ FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
@@ -1,5 +1,11 @@
 package com.how_hard_can_it_be.FirebaseAdmin;
 
+import java.io.FileInputStream;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredentials;
+
 /**
  * Hello world!
  *
@@ -7,7 +13,23 @@ package com.how_hard_can_it_be.FirebaseAdmin;
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    {       
+        try
+        {
+        FileInputStream serviceAccount = new FileInputStream(
+                "diaspora-dev-firebase-adminsdk-voul2-9fc9e89711.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+          .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+          .setDatabaseUrl("https://diaspora-dev.firebaseio.com/")
+          .build();
+
+        FirebaseApp.initializeApp(options);
+        }
+        catch (Exception exc)
+        {
+            System.err.println( "Error: " + exc.toString());
+        }
+        System.out.println( "(Firebase admin done.)");
     }
 }
