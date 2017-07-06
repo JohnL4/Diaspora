8bf0f701af4b44c0df3843c11fb99651b34d32fe
John Lusk <john-github@how-hard-can-it-be.com>
2017-07-06 19:45:11 -0400

    Dump out d/b to stdout.

--- FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
+++ FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
@@ -5,6 +5,11 @@ import java.io.FileInputStream;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Hello world!
@@ -12,24 +17,48 @@ import com.google.firebase.auth.FirebaseCredentials;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {       
-        try
-        {
-        FileInputStream serviceAccount = new FileInputStream(
-                "diaspora-dev-firebase-adminsdk-voul2-9fc9e89711.json");
+   public static void main( String[] args) throws InterruptedException
+   {
+      try
+      {
+         FileInputStream serviceAccount = new FileInputStream( "diaspora-dev-firebase-adminsdk-voul2-9fc9e89711.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-          .setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
-          .setDatabaseUrl("https://diaspora-dev.firebaseio.com/")
-          .build();
+         FirebaseOptions options = new FirebaseOptions.Builder()
+               .setCredential( FirebaseCredentials.fromCertificate( serviceAccount))
+               .setDatabaseUrl( "https://diaspora-dev.firebaseio.com/").build();
 
-        FirebaseApp.initializeApp(options);
-        }
-        catch (Exception exc)
-        {
-            System.err.println( "Error: " + exc.toString());
-        }
-        System.out.println( "(Firebase admin done.)");
-    }
+         FirebaseApp.initializeApp( options);
+
+         dumpClusterMetadata();
+      }
+      catch (Exception exc)
+      {
+         System.err.println( "Error: " + exc.toString());
+      }
+      Thread.sleep( 10000);
+      System.out.println( "(Firebase admin done.)");
+   }
+
+   private static void dumpClusterMetadata()
+   {
+      System.out.println( "Cluster metadata:");
+
+      DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
+      DatabaseReference kids = dbRef.child( "/clusters");
+
+      ValueEventListener listener = new ValueEventListener() {
+
+         public void onDataChange( DataSnapshot aSnapshot)
+         {
+            Object value = aSnapshot.getValue();
+            System.out.println( "\tgot value " + value);
+         }
+
+         public void onCancelled( DatabaseError aDbError)
+         {
+            System.err.println( "Cancelled.  " + aDbError);
+         }
+      };
+      kids.addValueEventListener( listener);
+   }
 }
