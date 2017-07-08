2121d5959c7ed229310851f943de2a0e7a000c1c
John Lusk <john-github@how-hard-can-it-be.com>
2017-07-07 22:36:44 -0400

    Restart w/new Maven project, since I screwed the old one up by attempting to package as an EAR file.

--- FirebaseAdmin/go.sh
+++ FirebaseAdmin/go.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-java -cp target/FirebaseAdmin-1.0-SNAPSHOT.jar com.how_hard_can_it_be.FirebaseAdmin.App
+java -cp target/FirebaseAdmin-1.0-SNAPSHOT.jar:$HOME/.m2/repository/com/google/firebase/firebase-admin/5.2.0/firebase-admin-5.2.0.jar \
+	com.how_hard_can_it_be.FirebaseAdmin.App
--- FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
+++ FirebaseAdmin/src/main/java/com/how_hard_can_it_be/FirebaseAdmin/App.java
@@ -1,70 +1,13 @@
 package com.how_hard_can_it_be.FirebaseAdmin;
 
-import java.io.FileInputStream;
-import java.util.concurrent.Semaphore;
-
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseCredentials;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
 /**
  * Hello world!
  *
  */
 public class App 
 {
-   private static Semaphore semaphore = new Semaphore( 0);
-   
-   public static void main( String[] args) throws InterruptedException
-   {
-      try
-      {
-         FileInputStream serviceAccount = new FileInputStream( "diaspora-dev-firebase-adminsdk-voul2-9fc9e89711.json");
-
-         FirebaseOptions options = new FirebaseOptions.Builder()
-               .setCredential( FirebaseCredentials.fromCertificate( serviceAccount))
-               .setDatabaseUrl( "https://diaspora-dev.firebaseio.com/").build();
-
-         FirebaseApp.initializeApp( options);
-
-         dumpClusterMetadata();
-      }
-      catch (Exception exc)
-      {
-         System.err.println( "Error: " + exc.toString());
-      }
-      semaphore.acquire();
-      System.out.println( "(Firebase admin done.)");
-   }
-
-   private static void dumpClusterMetadata()
-   {
-      System.out.println( "Cluster metadata:");
-
-      DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
-      DatabaseReference kids = dbRef.child( "/clusters");
-
-      ValueEventListener listener = new ValueEventListener() {
-
-         public void onDataChange( DataSnapshot aSnapshot)
-         {
-            Object value = aSnapshot.getValue();
-            System.out.println( "\tgot value " + value);
-            semaphore.release();
-         }
-
-         public void onCancelled( DatabaseError aDbError)
-         {
-            System.err.println( "Cancelled.  " + aDbError);
-            semaphore.release();
-         }
-      };
-      kids.addListenerForSingleValueEvent( listener);
-//      kids.addValueEventListener( listener);
-   }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
 }
